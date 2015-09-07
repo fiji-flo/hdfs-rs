fd2d2917796dfac84102c40377f8eb28b32b4018
Florian Merz <florian.merz@goeuro.com>
2015-09-07 14:25:09 +0200

    adopt env script

--- exec.sh
+++ exec.sh
@@ -6,12 +6,12 @@ if [ -z $HADOOP_HOME ]; then
 fi
 export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native
 
-HADOOP_MODULE_DIRS="$HADOOP_HOME/share/hadoop/common/lib/
-$HADOOP_HOME/share/hadoop/common/
+HADOOP_MODULE_DIRS=($HADOOP_HOME/share/hadoop/common/lib
+$HADOOP_HOME/share/hadoop/common
 $HADOOP_HOME/share/hadoop/hdfs
-$HADOOP_HOME/share/hadoop/hdfs/lib/
-$HADOOP_HOME/share/hadoop/yarn/lib/
-$HADOOP_HOME/share/hadoop/yarn/"
+$HADOOP_HOME/share/hadoop/hdfs/lib
+$HADOOP_HOME/share/hadoop/yarn/lib
+$HADOOP_HOME/share/hadoop/yarn)
 
 HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 CLASSPATH="${HADOOP_CONF_DIR}"
@@ -23,6 +23,6 @@ for d in $HADOOP_MODULE_DIRS; do
 done;
 
 export CLASSPATH
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${JAVA_HOME}/jre/lib/server
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${JAVA_HOME}/jre/lib/amd64/server
 
 $@
